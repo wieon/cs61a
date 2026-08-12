305c99b5bf246b72b63046ba2b3f2845c233c5e6
wieon <hfio-r15680@outlook.com>
2026-08-12 10:09:25 +0800

    Update disc06.py

--- discussion/disc06.py
+++ discussion/disc06.py
@@ -1,3 +1,6 @@
+import math
+
+# Q1 Keyboard
 LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
 
 class CapsLock:
@@ -86,4 +89,145 @@ class Keyboard:
 
 
 
+# Q2 Shape
+class Shape:
+    """All geometric shapes will inherit from this Shape class."""
+    def __init__(self, name):
+        self.name = name
+
+    def area(self):
+        """Returns the area of a shape"""
+        print("Override this method in ", type(self))
+
+    def perimeter(self):
+        """Returns the perimeter of a shape"""
+        print("Override this function in ", type(self))
+
+
+class Circle(Shape):
+    """A circle is characterized by its radii"""
+    def __init__(self, name, radius):
+        "*** YOUR CODE HERE ***"
+        # self.name = name
+        super().__init__(name)
+        self.radius = radius
+
+    def perimeter(self):
+        """Returns the perimeter of a circle (2πr)"""
+        "*** YOUR CODE HERE ***"
+        return 2 * math.pi * self.radius
+
+    def area(self):
+        """Returns the area of a circle (πr^2)"""
+        "*** YOUR CODE HERE ***"
+        return math.pi * self.radius * self.radius
+
+
+class RegPolygon(Shape):
+    """A regular polygon is defined as a shape whose angles and side lengths are all the same.
+    This means the perimeter is easy to calculate. The area can also be done, but it's more inconvenient."""
+    def __init__(self, name, num_sides, side_length):
+        "*** YOUR CODE HERE ***"
+        # self.name = name
+        super().__init__(name)
+        self.num_sides = num_sides
+        self.side_length = side_length
+
+    def perimeter(self):
+        """Returns the perimeter of a regular polygon (the number of sides multiplied by side length)"""
+        "*** YOUR CODE HERE ***"
+        return self.num_sides * self.side_length
+
+
+class Square(RegPolygon):
+    def __init__(self, name, side_length):
+        "*** YOUR CODE HERE ***"
+        # self.name = name
+        # self.side_length = side_length
+        super().__init__(name, 4, side_length)
+
+    def area(self):
+        """Returns the area of a square (squared side length)"""
+        "*** YOUR CODE HERE ***"
+        return self.side_length * self.side_length
     
+
+class Triangle(RegPolygon):
+    """An equilateral triangle"""
+    def __init__(self, name, side_length):
+        "*** YOUR CODE HERE ***"
+        # self.name = name
+        # self.side_length = side_length
+        super().__init__(name, 3, side_length)
+
+    def area(self):
+        """Returns the area of an equilateral triangle is (squared side length multiplied by the provided constant"""
+        constant = math.sqrt(3)/4
+        "*** YOUR CODE HERE ***"
+        return constant * self.side_length * self.side_length
+
+
+# Q3 Bear   
+class Eye:
+    """An eye.
+
+    >>> Eye().draw()
+    '0'
+    >>> print(Eye(False).draw(), Eye(True).draw())
+    0 -
+    """
+    def __init__(self, closed=False):
+        self.closed = closed
+
+    def draw(self):
+        if self.closed:
+            return '-'
+        else:
+            return '0'
+
+class Bear:
+    """A bear.
+
+    >>> Bear().print()
+    ? 0o0?
+    """
+    def __init__(self):
+        self.nose_and_mouth = 'o'
+
+    def next_eye(self):
+        return Eye()
+
+    def print(self):
+        left, right = self.next_eye(), self.next_eye()
+        print('? ' + left.draw() + self.nose_and_mouth + right.draw() + '?')
+
+
+class SleepyBear(Bear):
+    """A bear with closed eyes.
+
+    >>> SleepyBear().print()
+    ? -o-?
+    """
+    "*** YOUR CODE HERE ***"
+    def next_eye(self):
+        return Eye(True)
+
+
+class WinkingBear(Bear):
+    """A bear whose left eye is different from its right eye.
+
+    >>> WinkingBear().print()
+    ? -o0?
+    """
+    def __init__(self):
+        "*** YOUR CODE HERE ***"
+        super().__init__()
+        self.call_num = 0
+
+    def next_eye(self):
+        "*** YOUR CODE HERE ***"
+        self.call_num += 1
+        return Eye(self.call_num % 2)
+
+# print(WinkingBear().print())
+
